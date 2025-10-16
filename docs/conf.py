4e6d397578aaa4313c19b1cd11914bdca5068c3a
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2025-10-16 11:13:52 -0400

    release 0.0.7

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ author = 'Kwan Yuet Stephen Ho'
 # The short X.Y version.
 version = '0.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.0.6'
+release = '0.0.7'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
